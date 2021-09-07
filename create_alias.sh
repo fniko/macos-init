238fd6699c4762168dc89cbd3756ca14648e7a2f
filipniko <filip.niko25@gmail.com>
2021-09-07 19:05:13 +0200

    Save progress

--- create_alias.sh
+++ create_alias.sh
@@ -6,28 +6,30 @@ echo "######################"
 
 declare -a aliases_list
 
-# Just add alias in the following matrix and it will be added to ~/.bash_profile
+# Just add alias in the following matrix and it will be added to ~/.bashrc
 aliases_list=(
   "cls='clear'"
   "c='xclip'"
   "v='xclip -o'"
   "tf='terraform'"
-  "k='kubecetl'"
+  "k='kubectl'"
   "ll='ls -alF'"
   "la='ls -A'"
   "l='ls -CF'"
+  "tg='terragrunt'"
+  "p='python3'"
 )
 
 # Check if alias exists and if not add it
 for item in "${aliases_list[@]}"; do
-  if grep -Fxq "alias $item;" ~/.bash_profile; then
+  if grep -Fxq "alias $item;" ~/.bashrc; then
     echo "Skipping $item ..."
   else
     echo "Adding $item"
-    echo "alias $item;" >> ~/.bash_profile
+    echo "alias $item;" >> ~/.bashrc
   fi
 done
 
-echo "> Show content of ~/.bash_profile"
-cat ~/.bash_profile
+echo "> Show content of ~/.bashrc"
+cat ~/.bashrc
 echo "> [DONE] Aliases added!"
--- get_apps.sh
+++ get_apps.sh
@@ -8,16 +8,15 @@ echo "######################"
 
 # Add starship so it will load it upon bash startup; if not exists
 brew install starship
-grep -qxF 'eval "$(starship init bash)"' ~/.bash_profile || echo 'eval "$(starship init bash)"' >> ~/.bash_profile
-source ~/.bash_profile
+grep -qxF 'eval "$(starship init zsh)"' ~/.bashrc || echo 'eval "$(starship init zsh)"' >> ~/.bashrc
+source ~/.bashrc
 cp starship.toml ~/.config/starship.toml
-cat ~/.bash_profile
+cat ~/.bashrc
 echo "----------------"
 
 brew install bash
 brew install bash-completion
 
-brew install vim --override-system-vi
 brew install screen
 
 brew install tree
@@ -25,4 +24,20 @@ brew install watch
 brew install nano
 brew install htop
 brew install kubectl
+brew install kubectx
+brew install terraform
+brew install terragrunt
+brew install azure-cli
+brew install direnv
+brew install telnet
 
+# Cask
+brew install --cask boxcryptor
+brew install --cask dropbox
+brew install --cask qbittorrent
+brew install --cask nrlquaker-winbox
+brew install --cask pritunl
+brew install --cask opera
+brew install --cask firefox
+brew install --cask microsoft-remote-desktop
+brew install --cask spotify
